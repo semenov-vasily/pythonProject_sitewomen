65e7a63dff8f42b8fa2ed2c61cb299d5360c6caa
Semenov Vasily <vvs1771@gmail.com>
2024-06-06 11:09:52 +0300

    commit_1

---
+++ women/admin.py
@@ -0,0 +1,72 @@
+from django.contrib import admin, messages
+from django.utils.safestring import mark_safe
+
+from women.models import Women, Category, TagPost, Husband
+
+# class WomenTagsInline(admin.TabularInline):
+#     model = Women.tags.through
+#     extra = 1
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('title', 'post_photo', 'time_create', 'is_published', 'cat')
+    list_display_links = ('title',)
+    ordering = ['time_create', 'title']
+    list_editable = ('is_published', 'cat')
+    actions = ['set_published', 'set_draft']
+    search_fields = ['title__startswith', 'cat__name']
+    list_filter = [MarriedFilter, 'cat__name', 'is_published']
+    fields = ['title', 'content', 'photo', 'post_photo', 'slug', 'cat', 'husband', 'tags']
+    readonly_fields = ['post_photo']
+    prepopulated_fields = {"slug": ("title",)}
+    filter_horizontal = ['tags']
+    save_on_top = True
+
+    @admin.display(description="Изображение")
+    def post_photo(self, women: Women):
+        if women.photo:
+            return mark_safe(f"<img src='{women.photo.url}' width=50>")
+        return "Без фото"
+
+    @admin.action(description="Опубликовать выбранные записи")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Изменено {count}записи(ей).")
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(request, f"{count} записи(ей) сняты с публикации!", messages.WARNING)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+
+
+@admin.register(TagPost)
+class TagPostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tag', 'slug']
+
+
+@admin.register(Husband)
+class HusbandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'age', 'm_count']
---
+++ women/sitemaps.py
@@ -0,0 +1,21 @@
+from django.contrib.sitemaps import Sitemap
+from .models import Women, Category
+
+
+class PostSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.9
+
+    def items(self):
+        return Women.published.all()
+
+    def lastmod(self, obj):
+        return obj.time_update
+
+
+class CategorySitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.9
+
+    def items(self):
+        return Category.objects.all()
