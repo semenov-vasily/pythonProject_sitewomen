72edbd0a69e60b2db3b7898a31d8ed0585edf130
Semenov Vasily <vvs1771@gmail.com>
2024-06-13 19:25:39 +0300

    commit_27

--- women/views.py
+++ women/views.py
@@ -193,7 +193,7 @@ class ContactFormView(LoginRequiredMixin, DataMixin, FormView):
 
 
 def login(request):
-    return HttpResponse("Авторизация7")
+    return HttpResponse("Авторизация8")
 
 
 # def show_category(request, cat_slug):
